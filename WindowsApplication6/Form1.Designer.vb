8eb667506990cd1d456481bf9986efa5b424477c
callucam <callucam@gmail.com>
2013-10-25 16:23:40 -0700

    Initial

---
+++ WindowsApplication6/Form1.Designer.vb
@@ -0,0 +1,472 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TourName = New System.Windows.Forms.TextBox()
+        Me.ns2horizFov = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ns1heading = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ns1altitude = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ns1latitude = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.ns2altitudeMode = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.ns1range = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.ns1tilt = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.ns2flyToMode = New System.Windows.Forms.TextBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.ns2duration = New System.Windows.Forms.TextBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.ns1longitude = New System.Windows.Forms.TextBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ns1longitudeMax = New System.Windows.Forms.TextBox()
+        Me.ns1rangeMax = New System.Windows.Forms.TextBox()
+        Me.ns1tiltMax = New System.Windows.Forms.TextBox()
+        Me.ns1headingMax = New System.Windows.Forms.TextBox()
+        Me.ns1altitudeMax = New System.Windows.Forms.TextBox()
+        Me.ns1latitudeMax = New System.Windows.Forms.TextBox()
+        Me.ns1when = New System.Windows.Forms.DateTimePicker()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.PlaceMarkName = New System.Windows.Forms.TextBox()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.ModelName = New System.Windows.Forms.TextBox()
+        Me.OpenFileDialog2 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "TourName"
+        '
+        'TourName
+        '
+        Me.TourName.Location = New System.Drawing.Point(127, 16)
+        Me.TourName.Name = "TourName"
+        Me.TourName.Size = New System.Drawing.Size(208, 20)
+        Me.TourName.TabIndex = 1
+        Me.TourName.Text = "TourFromPlacemarkName"
+        '
+        'ns2horizFov
+        '
+        Me.ns2horizFov.Location = New System.Drawing.Point(127, 94)
+        Me.ns2horizFov.Name = "ns2horizFov"
+        Me.ns2horizFov.Size = New System.Drawing.Size(208, 20)
+        Me.ns2horizFov.TabIndex = 3
+        Me.ns2horizFov.Text = "60"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 97)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "ns2horizFov"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 123)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(50, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "ns1when"
+        '
+        'ns1heading
+        '
+        Me.ns1heading.Location = New System.Drawing.Point(127, 224)
+        Me.ns1heading.Name = "ns1heading"
+        Me.ns1heading.Size = New System.Drawing.Size(208, 20)
+        Me.ns1heading.TabIndex = 11
+        Me.ns1heading.Text = "160"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 227)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(62, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "ns1heading"
+        '
+        'ns1altitude
+        '
+        Me.ns1altitude.Location = New System.Drawing.Point(127, 198)
+        Me.ns1altitude.Name = "ns1altitude"
+        Me.ns1altitude.Size = New System.Drawing.Size(208, 20)
+        Me.ns1altitude.TabIndex = 9
+        Me.ns1altitude.Text = "12.9"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 201)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(58, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "ns1altitude"
+        '
+        'ns1latitude
+        '
+        Me.ns1latitude.Location = New System.Drawing.Point(127, 172)
+        Me.ns1latitude.Name = "ns1latitude"
+        Me.ns1latitude.Size = New System.Drawing.Size(208, 20)
+        Me.ns1latitude.TabIndex = 7
+        Me.ns1latitude.Text = "50.63394785"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 175)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(58, 13)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "ns1latitude"
+        '
+        'ns2altitudeMode
+        '
+        Me.ns2altitudeMode.Location = New System.Drawing.Point(127, 302)
+        Me.ns2altitudeMode.Name = "ns2altitudeMode"
+        Me.ns2altitudeMode.Size = New System.Drawing.Size(208, 20)
+        Me.ns2altitudeMode.TabIndex = 17
+        Me.ns2altitudeMode.Text = "relativeToSeaFloor"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(12, 305)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(85, 13)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "ns2altitudeMode"
+        '
+        'ns1range
+        '
+        Me.ns1range.Location = New System.Drawing.Point(127, 276)
+        Me.ns1range.Name = "ns1range"
+        Me.ns1range.Size = New System.Drawing.Size(208, 20)
+        Me.ns1range.TabIndex = 15
+        Me.ns1range.Text = "0.1"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(12, 279)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(51, 13)
+        Me.Label8.TabIndex = 14
+        Me.Label8.Text = "ns1range"
+        '
+        'ns1tilt
+        '
+        Me.ns1tilt.Location = New System.Drawing.Point(127, 250)
+        Me.ns1tilt.Name = "ns1tilt"
+        Me.ns1tilt.Size = New System.Drawing.Size(208, 20)
+        Me.ns1tilt.TabIndex = 13
+        Me.ns1tilt.Text = "80"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(12, 253)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(34, 13)
+        Me.Label9.TabIndex = 12
+        Me.Label9.Text = "ns1tilt"
+        '
+        'ns2flyToMode
+        '
+        Me.ns2flyToMode.Location = New System.Drawing.Point(127, 354)
+        Me.ns2flyToMode.Name = "ns2flyToMode"
+        Me.ns2flyToMode.Size = New System.Drawing.Size(208, 20)
+        Me.ns2flyToMode.TabIndex = 21
+        Me.ns2flyToMode.Text = "smooth"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(12, 357)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(74, 13)
+        Me.Label11.TabIndex = 20
+        Me.Label11.Text = "ns2flyToMode"
+        '
+        'ns2duration
+        '
+        Me.ns2duration.Location = New System.Drawing.Point(127, 328)
+        Me.ns2duration.Name = "ns2duration"
+        Me.ns2duration.Size = New System.Drawing.Size(208, 20)
+        Me.ns2duration.TabIndex = 19
+        Me.ns2duration.Text = "0.1"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(12, 331)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(62, 13)
+        Me.Label12.TabIndex = 18
+        Me.Label12.Text = "ns2duration"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'ns1longitude
+        '
+        Me.ns1longitude.Location = New System.Drawing.Point(127, 146)
+        Me.ns1longitude.Name = "ns1longitude"
+        Me.ns1longitude.Size = New System.Drawing.Size(208, 20)
+        Me.ns1longitude.TabIndex = 25
+        Me.ns1longitude.Text = "-117.928613537859"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(12, 149)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(67, 13)
+        Me.Label13.TabIndex = 24
+        Me.Label13.Text = "ns1longitude"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(341, 39)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(149, 23)
+        Me.Button1.TabIndex = 26
+        Me.Button1.Text = "Open Placemark"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(159, 401)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(149, 23)
+        Me.Button2.TabIndex = 27
+        Me.Button2.Text = "Create Tour"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ns1longitudeMax
+        '
+        Me.ns1longitudeMax.Location = New System.Drawing.Point(341, 146)
+        Me.ns1longitudeMax.Name = "ns1longitudeMax"
+        Me.ns1longitudeMax.Size = New System.Drawing.Size(208, 20)
+        Me.ns1longitudeMax.TabIndex = 33
+        Me.ns1longitudeMax.Text = "-117.928613537859"
+        '
+        'ns1rangeMax
+        '
+        Me.ns1rangeMax.Location = New System.Drawing.Point(341, 276)
+        Me.ns1rangeMax.Name = "ns1rangeMax"
+        Me.ns1rangeMax.Size = New System.Drawing.Size(208, 20)
+        Me.ns1rangeMax.TabIndex = 32
+        Me.ns1rangeMax.Text = "0.1"
+        '
+        'ns1tiltMax
+        '
+        Me.ns1tiltMax.Location = New System.Drawing.Point(341, 250)
+        Me.ns1tiltMax.Name = "ns1tiltMax"
+        Me.ns1tiltMax.Size = New System.Drawing.Size(208, 20)
+        Me.ns1tiltMax.TabIndex = 31
+        Me.ns1tiltMax.Text = "80"
+        '
+        'ns1headingMax
+        '
+        Me.ns1headingMax.Location = New System.Drawing.Point(341, 224)
+        Me.ns1headingMax.Name = "ns1headingMax"
+        Me.ns1headingMax.Size = New System.Drawing.Size(208, 20)
+        Me.ns1headingMax.TabIndex = 30
+        Me.ns1headingMax.Text = "180"
+        '
+        'ns1altitudeMax
+        '
+        Me.ns1altitudeMax.Location = New System.Drawing.Point(341, 198)
+        Me.ns1altitudeMax.Name = "ns1altitudeMax"
+        Me.ns1altitudeMax.Size = New System.Drawing.Size(208, 20)
+        Me.ns1altitudeMax.TabIndex = 29
+        Me.ns1altitudeMax.Text = "12.9"
+        '
+        'ns1latitudeMax
+        '
+        Me.ns1latitudeMax.Location = New System.Drawing.Point(341, 172)
+        Me.ns1latitudeMax.Name = "ns1latitudeMax"
+        Me.ns1latitudeMax.Size = New System.Drawing.Size(208, 20)
+        Me.ns1latitudeMax.TabIndex = 28
+        Me.ns1latitudeMax.Text = "50.63394785"
+        '
+        'ns1when
+        '
+        Me.ns1when.Location = New System.Drawing.Point(127, 120)
+        Me.ns1when.Name = "ns1when"
+        Me.ns1when.Size = New System.Drawing.Size(208, 20)
+        Me.ns1when.TabIndex = 34
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(12, 45)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(88, 13)
+        Me.Label10.TabIndex = 2
+        Me.Label10.Text = "Placemark Name"
+        '
+        'PlaceMarkName
+        '
+        Me.PlaceMarkName.Location = New System.Drawing.Point(127, 42)
+        Me.PlaceMarkName.Name = "PlaceMarkName"
+        Me.PlaceMarkName.Size = New System.Drawing.Size(208, 20)
+        Me.PlaceMarkName.TabIndex = 3
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(12, 71)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(67, 13)
+        Me.Label14.TabIndex = 2
+        Me.Label14.Text = "Model Name"
+        '
+        'ModelName
+        '
+        Me.ModelName.Location = New System.Drawing.Point(127, 68)
+        Me.ModelName.Name = "ModelName"
+        Me.ModelName.Size = New System.Drawing.Size(208, 20)
+        Me.ModelName.TabIndex = 3
+        '
+        'OpenFileDialog2
+        '
+        Me.OpenFileDialog2.FileName = "OpenFileDialog2"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(341, 71)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(149, 23)
+        Me.Button3.TabIndex = 35
+        Me.Button3.Text = "Open Model"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(684, 492)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ns1when)
+        Me.Controls.Add(Me.ns1longitudeMax)
+        Me.Controls.Add(Me.ns1rangeMax)
+        Me.Controls.Add(Me.ns1tiltMax)
+        Me.Controls.Add(Me.ns1headingMax)
+        Me.Controls.Add(Me.ns1altitudeMax)
+        Me.Controls.Add(Me.ns1latitudeMax)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ns1longitude)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.ns2flyToMode)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.ns2duration)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.ns2altitudeMode)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.ns1range)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.ns1tilt)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.ns1heading)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.ns1altitude)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.ns1latitude)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.PlaceMarkName)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.ModelName)
+        Me.Controls.Add(Me.ns2horizFov)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TourName)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TourName As System.Windows.Forms.TextBox
+    Friend WithEvents ns2horizFov As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents ns1heading As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents ns1altitude As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents ns1latitude As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents ns2altitudeMode As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents ns1range As System.Windows.Forms.TextBox
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents ns1tilt As System.Windows.Forms.TextBox
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents ns2flyToMode As System.Windows.Forms.TextBox
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents ns2duration As System.Windows.Forms.TextBox
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents ns1longitude As System.Windows.Forms.TextBox
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents ns1longitudeMax As System.Windows.Forms.TextBox
+    Friend WithEvents ns1rangeMax As System.Windows.Forms.TextBox
+    Friend WithEvents ns1tiltMax As System.Windows.Forms.TextBox
+    Friend WithEvents ns1headingMax As System.Windows.Forms.TextBox
+    Friend WithEvents ns1altitudeMax As System.Windows.Forms.TextBox
+    Friend WithEvents ns1latitudeMax As System.Windows.Forms.TextBox
+    Friend WithEvents ns1when As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents PlaceMarkName As System.Windows.Forms.TextBox
+    Friend WithEvents Label14 As System.Windows.Forms.Label
+    Friend WithEvents ModelName As System.Windows.Forms.TextBox
+    Friend WithEvents OpenFileDialog2 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+
+End Class
---
+++ WindowsApplication6/Form1.vb
@@ -0,0 +1,157 @@
+﻿#Region "Imports directives"
+
+Imports System.Reflection
+Imports System.IO
+Imports Excel = Microsoft.Office.Interop.Excel
+Imports System.Runtime.InteropServices
+Imports Microsoft.VisualBasic
+
+#End Region
+
+
+Public Class Form1
+
+    Dim XPlaceMark As XElement
+    Dim XModel As XElement
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim oXL As Excel.Application = Nothing
+        Dim oWBs As Excel.Workbooks = Nothing
+        Dim oWB As Excel.Workbook = Nothing
+        'Dim NameSheet As Excel.Worksheet = Nothing
+        'Dim LookAtSheet As Excel.Worksheet = Nothing
+        'Dim PlaceMarkSheet As Excel.Worksheet = Nothing
+
+        Dim oCells As Excel.Range = Nothing
+        oXL = New Excel.Application
+        oXL.Visible = False
+        oWBs = oXL.Workbooks
+        'oWB = oWBs.Open("C:\Resource Documents\Resources\Simulation\Google Earth Animation\SetupViewFromLocation.xlsm")
+        'NameSheet = oWB.Worksheets(1)
+        'LookAtSheet = oWB.Worksheets(2)
+        'PlaceMarkSheet = oWB.Worksheets(3)
+
+        oWB = oWBs.Open("C:\Resource Documents\Resources\Simulation\Google Earth Animation\Template.xlsx")
+        Dim Document As Excel.Worksheet = oWB.Worksheets(1)
+        'Dim LookAtSheet1 As Excel.Worksheet = oWB.Worksheets(2)
+        'Dim Style1 As Excel.Worksheet = oWB.Worksheets(3)
+        Dim Folder As Excel.Worksheet = oWB.Worksheets(2)
+        'Dim LookAt2 As Excel.Worksheet = oWB.Worksheets(5)
+        'Dim Style2 As Excel.Worksheet = oWB.Worksheets(6)
+        Dim PlaceMark As Excel.Worksheet = oWB.Worksheets(3)
+        Dim Tour As Excel.Worksheet = oWB.Worksheets(4)
+        Dim FlyTo As Excel.Worksheet = oWB.Worksheets(5)
+
+        'Tour.Range("a1").Value = TourName.Text
+
+        Dim m2 As Date = ns1when.Value
+        Dim m3 As Date = CDate(Date.FromOADate(CDbl(ns1when.Value.ToOADate()) + 1 / 60 / 60 / 24))
+        Dim RowCount As Integer = 100
+
+        Dim horizFov As String = ns2horizFov.Text
+        Dim longitude As Double = ns1longitude.Text
+        Dim longitudeMax As Double = ns1longitudeMax.Text
+        Dim latitude As Double = ns1latitude.Text
+        Dim latitudeMax As Double = ns1latitudeMax.Text
+        Dim altitude As Double = ns1altitude.Text
+        Dim altitudeMax As Double = ns1altitudeMax.Text
+        Dim heading As Double = ns1heading.Text
+        Dim headingMax As Double = ns1headingMax.Text
+        Dim tilt As Double = ns1tilt.Text
+        Dim tiltMax As Double = ns1tiltMax.Text
+        Dim range As Double = ns1range.Text
+        Dim rangeMax As Double = ns1rangeMax.Text
+        Dim altitudeMode As String = ns2altitudeMode.Text
+        Dim duration As Double = ns2duration.Text
+        Dim flyToMode As String = ns2flyToMode.Text
+
+        For i = 0 To RowCount
+            FlyTo.Range("a2").Offset(i, 0).Value = altitudeMode
+            FlyTo.Range("b2").Offset(i, 0).Value = horizFov
+            FlyTo.Range("c2").Offset(i, 0).Value = Year(m2) & "-" & Format(Month(m2), "00") & "-" & Format(Day(m2), "00") & "T" & Format(Hour(m2), "00") & ":" & Format(Minute(m2), "00") & ":" & Format(Second(m2), "00") & "Z"
+            FlyTo.Range("d2").Offset(i, 0).Value = Year(m2) & "-" & Format(Month(m2), "00") & "-" & Format(Day(m2), "00") & "T" & Format(Hour(m2), "00") & ":" & Format(Minute(m2), "00") & ":" & Format(Second(m2), "00") & "Z"
+            FlyTo.Range("e2").Offset(i, 0).Value = longitude + (longitudeMax - longitude) / RowCount * i
+            FlyTo.Range("f2").Offset(i, 0).Value = latitude + (latitudeMax - latitude) / RowCount * i
+            FlyTo.Range("g2").Offset(i, 0).Value = altitude + (altitudeMax - altitude) / RowCount * i
+            FlyTo.Range("h2").Offset(i, 0).Value = heading + (headingMax - heading) / RowCount * i
+            FlyTo.Range("i2").Offset(i, 0).Value = tilt + (tiltMax - tilt) / RowCount * i
+            FlyTo.Range("j2").Offset(i, 0).Value = range + (rangeMax - range) / RowCount * i
+            FlyTo.Range("k2").Offset(i, 0).Value = duration
+            FlyTo.Range("l2").Offset(i, 0).Value = flyToMode
+        Next
+
+
+        Dim MyPath As String = "C:\Resource Documents\Resources\Simulation\Google Earth Animation\"
+        Dim MyFile As String = TourName.Text & " " & Year(m2) & Format(Month(m2), "00") & Format(Day(m2), "00") & " " & Format(Hour(m2), "00") & Format(Minute(m2), "00") & Format(Second(m2), "00") & ".xml"
+        Dim NewName As String = TourName.Text & " " & Year(m2) & Format(Month(m2), "00") & Format(Day(m2), "00") & " " & Format(Hour(m2), "00") & Format(Minute(m2), "00") & Format(Second(m2), "00") & ".kml"
+
+        Dim zz As Excel.XmlMap = oWB.XmlMaps("kml_Map")
+
+        oWB.SaveAsXMLData(MyPath & MyFile, zz)
+
+        oWB.Close(False)
+
+        If Dir(MyPath & MyFile) <> "" Then
+            My.Computer.FileSystem.RenameFile(MyPath & MyFile, NewName)
+            'Name MyPath & MyFile As MyPath & NewName
+        Else
+            MsgBox("File not found")
+        End If
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim initialDirectory As String = "C:\Resource Documents\Resources\Simulation\Google Earth Animation\"
+        OpenFileDialog1.InitialDirectory = initialDirectory
+        OpenFileDialog1.FileName = ""
+        OpenFileDialog1.DefaultExt = "kml"
+        OpenFileDialog1.AddExtension = True
+        OpenFileDialog1.Filter = "KML files (*.kml)|*.kml|All files (*.*)|*.*"
+        OpenFileDialog1.ShowDialog()
+
+        Dim k As XNamespace = "http://www.opengis.net/kml/2.2"
+
+        PlaceMarkName.Text = OpenFileDialog1.FileName
+
+        XPlaceMark = XElement.Load(OpenFileDialog1.FileName)
+
+        Dim coordinates As String = (XPlaceMark.Elements(k + "Document").Elements(k + "Placemark").Elements(k + "Point").Elements(k + "coordinates").FirstOrDefault)
+
+        Dim firstcomma As Integer = (InStr(coordinates, ","))
+
+        Dim longitude As Double = Microsoft.VisualBasic.Left(coordinates, firstcomma - 1)
+
+        coordinates = Microsoft.VisualBasic.Right(coordinates, Len(coordinates) - firstcomma)
+
+        firstcomma = (InStr(coordinates, ","))
+
+        Dim lattitude As Double = Microsoft.VisualBasic.Left(coordinates, firstcomma - 1)
+
+        Dim altitude As Double = Microsoft.VisualBasic.Right(coordinates, Len(coordinates) - firstcomma)
+
+        ns1longitude.Text = longitude
+        ns1latitude.Text = lattitude
+        ns1altitude.Text = altitude
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim initialDirectory As String = "C:\Resource Documents\Resources\Simulation\Google Earth Animation\"
+        OpenFileDialog2.InitialDirectory = initialDirectory
+        OpenFileDialog2.FileName = ""
+        OpenFileDialog2.DefaultExt = "kml"
+        OpenFileDialog2.AddExtension = True
+        OpenFileDialog2.Filter = "KML files (*.kml)|*.kml|All files (*.*)|*.*"
+        OpenFileDialog2.ShowDialog()
+
+        Dim k As XNamespace = "http://www.opengis.net/kml/2.2"
+
+        ModelName.Text = OpenFileDialog2.FileName
+
+        XModel = XElement.Load(OpenFileDialog2.FileName)
+
+    End Sub
+End Class
